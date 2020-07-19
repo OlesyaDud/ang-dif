69f31122b18bed8e922a256222ee05b9a6c85813
OlesyaDud <wodadevelopment@gmail.com>
2020-07-19 09:59:19 -0400

    child routes. sidebar with hover

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, 
+  children: [
+    { path: 'blog', component: BlogComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'contact', component: ContactComponent },
+  ]},
+     { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    ButtonComponent,
+    CardComponent,
+    JumbotronComponent, 
+    HomeComponent
+  ],
+ 
   providers: [],
   bootstrap: [AppComponent]
 })
